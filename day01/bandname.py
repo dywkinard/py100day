caa63699c844a25290d0a17483c36f0d843e260d
Dylan <dylan.kinard@icloud.com>
2022-09-29 12:16:33 -0700

    Completed day 1

---
+++ day01/bandname.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("What city did you grow up in? \n")
+pet = input("What is your pets name? \n")
+print(f"Your band name could be {city} {pet}!")
