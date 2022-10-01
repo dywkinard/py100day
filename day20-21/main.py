8ff5874931604b7182fa727039a92b6132b25186
Dylan <dylan.kinard@icloud.com>
2022-09-30 17:16:38 -0700

    Completed day 20

---
+++ day20-21/main.py
@@ -0,0 +1,24 @@
+from snake import Snake
+from turtle import Screen, Turtle
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('Snake Game')
+screen.tracer(0)
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
+game_over = False
+while not game_over:
+    screen.update()
+    time.sleep(.1)
+    snake.move()
+
+screen.exitonclick()
